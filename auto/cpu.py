cfa9a9f021acae2cbcdd86ac0bcd139c088dc0d1
Daniel Kostecki <dkosteck@redhat.com>
2021-09-10 16:40:56 -0400

    Locally working update_testfile function, which replaces the setcore in config files with one based off the current system. Needs more unit testing and testing on other machines, but working on a Lenovo laptop

--- auto/cpu.py
+++ auto/cpu.py
@@ -179,9 +179,9 @@ class CpuResource:
 class Setting:
     def __init__(self, cfg):
         try:
-            self.f = open(cfg, 'r+')
-            self.cfg = list(self.f)
-            #f.close()
+            f = open(cfg, 'r')
+            self.cfg = list(f)
+            f.close()
         except:
             sys.exit("can't process %s" %(cfg))
 
@@ -198,20 +198,41 @@ class Setting:
     # reference https://docs.google.com/document/d/1CLlfh2pt2eOxwus0gnOXuAnGT9yRYVu0Rrr8wTAP_0I/edit?usp=sharing
         pass
 
+    # A function to take the setcore maps in the config file and allocate
+    # relevant cpus on the current machine, creating a new map and writing it
+    # in place, always allocating siblings.
+    #
+    # References:
+    #   https://stackoverflow.com/questions/38935169/convert-elements-of-a-list-into-binary
+    #   https://stackoverflow.com/questions/21409461/binary-list-from-indices-of-ascending-integer-list
     def update_testfile(self, rsc, testfile):
-        #print(self.cfg)
+        line_index = 0
         for line in self.cfg:
-            # Note: Not try catched.
             if 'setcore' in line:
                 setcore_index = line.index('setcore')
                 # The number of cpus (cores or threads, depening on hyperthreading, needed.)
                 # Take the string, convert to hex, convert to binary, count the 1s.
                 num_cpus = (bin(int(line[setcore_index + len('setcore'):].strip(), 16))[2:]).count('1')
-                print(rsc.allocate_siblings(num_cpus))
+                print(num_cpus)
+                # Allocate siblings based on the current setcore.
+                cpus = rsc.allocate_siblings(num_cpus)
+                # Create a list of binary digits based off CPU indices.
+                new_setcore_list = [int(i in cpus) for i in range(max(cpus)+1)]
+                # Revere the list, then create the binary number.
+                new_setcore_list.reverse()
+                new_setcore_binary = 0
+                for digit in new_setcore_list:
+                    new_setcore_binary = 2 * new_setcore_binary + digit
+                # Create the hex representation and replace the old setcore
+                new_setcore_hex = ' ' + hex(new_setcore_binary) + '\n'
+                self.cfg[line_index] = line.replace(line[setcore_index + len('setcore'):], new_setcore_hex)
 
-	# try to use allocate_siblings to allocate sibling threads
-	# reference https://docs.google.com/document/d/1CLlfh2pt2eOxwus0gnOXuAnGT9yRYVu0Rrr8wTAP_0I/edit?usp=sharing
-        self.f.close()
+            line_index += 1
+
+        # Write the new configuration to the same file.
+        f = open(testfile, 'w')
+        f.writelines(self.cfg)
+        f.close()
 
 def main(name, argv):
     nosibling = False
