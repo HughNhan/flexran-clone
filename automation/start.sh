cfaf6c26e769434f6714a5bc8da307aa8008acb3
jianzzha <jianzzha@redhat.com>
2021-12-15 16:13:17 -0500

    Increase timeout

--- automation/start.sh
+++ automation/start.sh
@@ -66,7 +66,7 @@ fi
 #cp -R pod/cfg_examples/timer_mode_cfg_ns3000_debug.yaml pod/timer_mode_cfg.yaml
 #### End of dci debug @80
 sleep 5
-./pod/pod_exec_updates.py -p flexran-du -d /opt/flexran/auto -c ./pod/timer_mode_cfg.yaml -f ./pod/autotest.py -f ./pod/cpu.py  -f ./pod/pod_exec_updates.py -f ./pod/process_testfile.py -f ./pod/read_yaml_write_xml.py -f ./pod/timer_mode_cfg.yaml -namespace ${FLEXRAN_DU_NS} -timeout 30
+./pod/pod_exec_updates.py -p flexran-du -d /opt/flexran/auto -c ./pod/timer_mode_cfg.yaml -f ./pod/autotest.py -f ./pod/cpu.py  -f ./pod/pod_exec_updates.py -f ./pod/process_testfile.py -f ./pod/read_yaml_write_xml.py -f ./pod/timer_mode_cfg.yaml -namespace ${FLEXRAN_DU_NS} -timeout 60
 
 if [[ "${RUN_XRAN:-false}" == "false" ]]; then
     echo "test complete"
@@ -98,7 +98,7 @@ echo "setup ru ..."
 ./du_pod_install.sh -x
 
 sleep 5
-./pod/pod_exec_updates.py -p flexran-du -d /opt/flexran/auto -c ./pod/xran_mode_cfg.yaml -f ./pod/autotest.py -f ./pod/cpu.py  -f ./pod/pod_exec_updates.py -f ./pod/process_testfile.py -f ./pod/read_yaml_write_xml.py -f ./pod/xran_mode_cfg.yaml --xran --phystart -namespace ${FLEXRAN_DU_NS} -timeout 30
+./pod/pod_exec_updates.py -p flexran-du -d /opt/flexran/auto -c ./pod/xran_mode_cfg.yaml -f ./pod/autotest.py -f ./pod/cpu.py  -f ./pod/pod_exec_updates.py -f ./pod/process_testfile.py -f ./pod/read_yaml_write_xml.py -f ./pod/xran_mode_cfg.yaml --xran --phystart -namespace ${FLEXRAN_DU_NS} -timeout 60
 
 echo "Flexran tests finish at the end"
 exit 0
